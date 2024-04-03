2206a7a7c0111e0fc7ca07d5105abbe7b1076261
Loaiabdalslam <32424891+loaiabdalslam@users.noreply.github.com>
2024-04-03 22:01:08 +0200

    Initial commit

---
+++ task.py
@@ -0,0 +1,32 @@
+Coding Task:
+Create a class called "Employee" that has the following attributes:
+- Name (string)
+- Age (integer)
+- Department (string)
+- Salary (float)
+
+Include a constructor method that initializes all attributes.
+Include a method called "getSalaryBonus" that calculates and returns a bonus of 10% of the salary.
+Create an object of the "Employee" class with the following details:
+Name: John Doe
+Age: 30
+Department: Marketing
+Salary: $50000
+
+Create a subclass of "Employee" called "Manager" that has an additional attribute:
+- Bonus (float)
+
+Include a constructor method that initializes all attributes including the bonus.
+Override the "getSalaryBonus" method to calculate a bonus of 20% of the salary.
+Create an object of the "Manager" class with the following details:
+Name: Jane Smith
+Age: 35
+Department: Sales
+Salary: $60000
+Bonus: $10000
+
+Create a function outside of the classes called "printEmployeeDetails" that takes an Employee object as a parameter and prints out all the details of the employee.
+
+Create an instance of each class and call the "printEmployeeDetails" function for each object.
+
+Note: Do not provide the code, only describe the coding task for the candidate to complete.
